fe83f3bf71040dcd7db54a2842c7411730a7305e
Erik Bylund <erik@lootlocker.io>
2022-10-12 14:53:57 +0200

    Add Tests for Apple Sign In, some need manual intervention though so they are ignored

--- Assets/SDKPortal.cs
+++ Assets/SDKPortal.cs
@@ -10,9 +10,22 @@ public class SDKPortal : MonoBehaviour
         LootLockerSDKManager.SubmitScore(memberId, score, leaderBoardKey, metadata, onComplete);
     }
 
-    
     public void SubmitScore(string memberId, int score, int leaderBoardId, string metadata, Action<LootLockerSubmitScoreResponse> onComplete) {
         LootLockerSDKManager.SubmitScore(memberId, score, leaderBoardId, metadata, onComplete);
     }
 
+    public void StartAppleSession(string authorizationCode, Action<LootLockerAppleSessionResponse> onComplete)
+    {
+        LootLockerSDKManager.StartAppleSession(authorizationCode, onComplete);
+    }
+
+    public void RefreshAppleSession(string refreshToken, Action<LootLockerAppleSessionResponse> onComplete)
+    {
+        LootLockerSDKManager.RefreshAppleSession(refreshToken, onComplete);
+    }
+
+    public void EndSession(Action<LootLockerSessionResponse> onComplete)
+    {
+        LootLockerSDKManager.EndSession(onComplete);
+    }
 }
