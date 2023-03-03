a07445738367771f41372517cd53f459f38b2995
Erik Bylund <erik@lootlocker.io>
2023-03-03 16:34:31 +0100

    Add Steam Authentication Test

--- Assets/Tests/PlayModeTests/GuestLoginTest.cs
+++ Assets/Tests/PlayModeTests/GuestLoginTest.cs
@@ -4,32 +4,101 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using LootLocker.Requests;
+using UnityEditor;
 
 namespace Tests
 {
     public class GuestLoginTest
     {
+        private static LootLockerConfig.DebugLevel debugLevel;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            LLTestUtils.InitSDK();
+            debugLevel = LootLockerConfig.current.currentDebugLevel;
+        }
+
+        [UnitySetUp]
+        public IEnumerator UnitySetUp()
+        {
+            yield return null;
+        }
+
         [UnityTearDown]
         public IEnumerator UnityTearDown()
         {
             // Cleanup
-            bool cleanupComplete = false;
             LootLockerConfig.current.currentDebugLevel = LootLockerConfig.DebugLevel.AllAsNormal;
-            LootLockerSDKManager.EndSession((response) => {
-                LootLockerConfig.current.currentDebugLevel = LootLockerConfig.DebugLevel.All; 
-                cleanupComplete = true;
+            bool cleanupComplete = false;
+            LootLockerSDKManager.DeletePlayer(deletePlayerResponse =>
+            {
+                if (!deletePlayerResponse.success)
+                {
+                    Debug.LogError("Failed to delete player: " + deletePlayerResponse.text);
+                    LootLockerSDKManager.EndSession(endSessionResponse =>
+                    {
+                        cleanupComplete = true;
+                    });
+                }
+                else
+                {
+                    cleanupComplete = true;
+                }
             });
             yield return new WaitUntil(() => cleanupComplete);
+            LootLockerConfig.current.currentDebugLevel = debugLevel;
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
         }
 
         [UnityTest]
-        public IEnumerator GuestUserCanLogIn()
+        public IEnumerator GuestUserCanLoginWithoutPlayerId()
         {
-            var guestLoginGO = new GameObject();
-            var guestLogin = guestLoginGO.AddComponent<GuestLogin>();
-            yield return new WaitUntil(() => guestLogin.IsDone());
-            Assert.IsTrue(guestLogin.IsLoggedIn());
-            Assert.IsTrue(guestLogin.GetPlayerId() != 0);
+            // Given
+            bool guestLoginSucceeded = false;
+            bool requestCompleted = false;
+
+            // When
+            LootLockerSDKManager.StartGuestSession(response =>
+            {
+                guestLoginSucceeded = response.success;
+                requestCompleted = true;
+            });  
+
+            // Wait for response
+            yield return new WaitUntil(() => requestCompleted);
+
+            // Then
+            Assert.IsTrue(guestLoginSucceeded, "Guest login failed");
+        }
+
+        [UnityTest]
+        public IEnumerator GuestUserCanLoginWithPlayerId()
+        {
+            // Given
+            bool guestLoginSucceeded = false;
+            bool requestCompleted = false;
+            string expectedPlayerIdentifier = GUID.Generate().ToString();
+            string actualPlayerIdentifier = "";
+
+            // When
+            LootLockerSDKManager.StartGuestSession(expectedPlayerIdentifier, response =>
+            {
+                guestLoginSucceeded = response.success;
+                requestCompleted = true;
+                actualPlayerIdentifier = response.player_identifier;
+            });
+
+            // Wait for response
+            yield return new WaitUntil(() => requestCompleted);
+
+            // Then
+            Assert.IsTrue(guestLoginSucceeded, "Guest login failed");
+            Assert.AreEqual(expectedPlayerIdentifier, actualPlayerIdentifier, "Player identifier was not as expected");
         }
     }
 }
---
+++ Assets/Tests/PlayModeTests/SteamAuthenticationTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using LootLocker;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using LootLocker.Requests;
+using Steamworks;
+using System;
+using System.Text;
+
+namespace Tests
+{
+    public class SteamAuthenticationTests
+    {
+        private static LootLockerConfig.DebugLevel debugLevel;
+        private static GameObject SteamManagerGO;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Debug.Log("One Time Setup");
+            LLTestUtils.InitSDK();
+            debugLevel = LootLockerConfig.current.currentDebugLevel;
+
+            // Setup Steam Manager
+            SteamManagerGO = new GameObject("SteamManager");
+            SteamManagerGO.AddComponent<SteamManager>();
+            if (!SteamManager.Initialized)
+            {
+                Debug.LogError("SteamManager Failed to initialize");
+            }
+            //SignInWithSteam();
+        }
+
+        [UnitySetUp]
+        public IEnumerator UnitySetUp()
+        {
+            //yield return new WaitUntil(() => m_IsSteamInitialized);
+            yield return null;
+        }
+
+        [UnityTearDown]
+        public IEnumerator UnityTearDown()
+        {
+            // Cleanup
+            LootLockerConfig.current.currentDebugLevel = debugLevel;
+            bool cleanupComplete = false;
+            LootLockerSDKManager.EndSession((response) =>
+            {
+                cleanupComplete = true;
+            });
+            yield return new WaitUntil(() => cleanupComplete);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [UnityTest]
+        [Ignore("No steam account set up without Steam Guard, so CI env can't run the test as a signed in client is needed")]
+        public IEnumerator SteamLoginSucceeds()
+        {
+            // Given
+            bool requestCompleted = false;
+            LootLockerSessionResponse sessionResponse = null;
+
+            // When
+            var ticket = new byte[1024];
+            SteamUser.GetAuthSessionTicket(ticket, 1024, out uint ticketSize);
+            Array.Resize(ref ticket, (int)ticketSize);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ticketSize; i++)
+            {
+                sb.AppendFormat("{0:x2}", ticket[i]);
+            }
+
+            var steamSessionTicket = sb.ToString();
+
+            LootLockerSDKManager.VerifySteamID(steamSessionTicket, verifySteamIdResponse =>
+            {
+                if (verifySteamIdResponse.success)
+                {
+                    LootLockerSDKManager.StartSteamSession(SteamUser.GetSteamID().ToString(),
+                        startSteamSessionResponse =>
+                        {
+                            sessionResponse = startSteamSessionResponse;
+                            requestCompleted = true;
+                        });
+                }
+                else
+                {
+                    requestCompleted = true;
+                    Assert.Fail("Failed to verify steam id");
+                }
+            });
+
+            // Wait for response
+            yield return new WaitUntil(() => requestCompleted);
+
+            // Then
+            Assert.IsNotNull(sessionResponse);
+            Assert.IsTrue(sessionResponse.success, "Failed to start steam session");
+            Assert.AreNotEqual(0, sessionResponse.player_id, "Player ID not set");
+        }
+    }
+}
